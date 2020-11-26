111dd8e5694c976f919e4d2b1a20103c46d94a3c
Karmo <karmorg@gmail.com>
2020-11-26 10:19:40 +0200

    Register.vue leht tööta

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from '../views/Register.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +20,17 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: function () {
+    //   return import(/* webpackChunkName: "about" */ '../views/Register.vue')
+    // }
   }
 ]
 
---
+++ src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Registreerige uus kasutaja või sisestage oma andmed sisse logimiseks"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+//
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
