fe8c88713488838c6b278b0cdc88d3cbf7d18f65
KadTar <73937863+KadTar@users.noreply.github.com>
2020-12-02 10:58:08 +0200

    TeenusteValimine ServiceName lisatud tabelisse

--- src/views/TeenusteValimine.vue
+++ src/views/TeenusteValimine.vue
@@ -3,9 +3,9 @@
     <h1>Teenuste valimine</h1>
     <br><br>
     <p>
-<!--      <button v-on:click="test()">vajuta</button>-->
+      <!--      <button v-on:click="test()">vajuta</button>-->
     </p>
-    <p align="right" >
+    <p align="right">
       <button v-on:click="test2()">salvesta</button>
     </p>
     <table border="1">
@@ -14,95 +14,74 @@
         <th>RegNr</th>
       </tr>
       <tr>
-        <td>siia on vaja midagi, mis tooks andmed </td>
+        <td>siia on vaja midagi, mis kuvaks andmed</td>
         <td>ja siia ka</td>
       </tr>
     </table>
     <br><br>
     <table border="1" align="center">
       <tr>
-        <th width="20">Index tuleb javascriptist ise</th>
-        <th width="30">ID</th>
-        <th width="30">AutoID</th>
+        <th width="30"></th>
+        <th hidden width="30">ID</th>
+        <th hidden width="30">AutoID</th>
         <th>Teenus</th>
         <th>Välba ühik</th>
         <th>Välba väärtus</th>
         <th>Märkus</th>
-        <th>Staatus</th>
-        <th>Valitud</th>
+        <th>Valitud/staatus</th>
+
       </tr>
       <tr v-for="(row, index) in resultList">
-        <td>{{index+1}}</td>
-        <td>{{row.id}}</td>
-        <td>{{row.vehicleId}}</td>
-        <td>{{row.serviceId}}</td>
-        <td><input v-model="row.pUnit"></td>
+        <td>{{ index + 1 }}</td>
+        <td hidden>{{ row.id }}</td>
+        <td hidden>{{ row.vehicleId }}</td>
+        <td>{{ row.serviceName }}</td>
+        <td><span>{{ row.pUnit }}</span><select v-model="row.pUnit">
+          <option>km</option>
+          <option>aasta</option>
+          <option>kontrollida</option>
+        </select></td>
         <td><input v-model="row.pValue"></td>
         <td><input v-model="row.comment"></td>
         <td><input type="checkbox" id="checkbox" v-model="row.active">
-          <label for="checkbox">{{row.active}}</label> </td>
-        <td><button v-on:click="valikuKinnitamine(index)"  >Vali</button></td>
+          <label hidden for="checkbox">{{ row.active }}</label></td>
+
       </tr>
     </table>
     <br><br><br><br>
-
   </div>
-
 </template>
 
 <script>
-let test = function () {
-  this.resultList.push({})
-}
-let kustutada=function (index){
-  this.resultList.splice(index, 1)
-}
 
 let getData = function () {
-  let url="http://localhost:8080/VehicleServiceList?vehicleId=30";
+  let url = "http://localhost:8080/VehicleServiceListWithServiceName?vehicleId=8";
 
   this.resultList = this.$http.get(url)
-      .then(result => this.resultList=result.data)
-
+      .then(result => this.resultList = result.data)
 }
-// let showresponse = function (response) {
-//   this.resultList = response.data;
-// }
-let test2 = function (index){
-  alert(this.serviceList[index].id);
+
+let test2 = function () {
   let url = "http://localhost:8080/updateSelectedService";
 
-  let body = {
-    id: this.serviceList[index].id,
-
-
-  }
-  this.$http.put(url, body)
+  this.$http.put(url, this.resultList)
       .then(alert("uuendatud"))
 
 }
 
-
-
 export default {
-  //name:'Demo',
-  methods: {//defineerid meetodid
-    test: test,
-//    kustutada:kustutada,
+  methods: {
     getData: getData,
-    test2:test2,
-    // showresponse: showresponse
+    test2: test2,
   },
-
-  data: function () {//tagastab objeti, mille sees on muutujad mida saab kasutada
+  data: function () {
     return {
       resultList: []
     }
   },
-  created() {//mis pannakse automaatselt tööle kui lähed lehele (kõige esimesena) Kuvab listi/info vaikimisi kohe
+  created() {
     this.getData()
   }
-
 }
 </script>
 
